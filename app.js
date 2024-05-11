500d33405c0cd948e0042b200d6bbf88e462010c
quewon <36914642+quewon@users.noreply.github.com>
2024-05-11 10:44:36 +0900

    .

--- app.js
+++ app.js
@@ -544,7 +544,7 @@ const server = http.createServer(app)
 const { Server } = require('socket.io')
 const io = new Server(server)
 
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'))
 
